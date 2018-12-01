18e15db0e75caf24f9dde6c12090b9fe8d8bd6dd
vistersky <vistersky@gmail.com>
2018-12-01 10:46:27 +0100

    Hello World commit

---
+++ src/main/java/pl/vistersky/helloworld/HelloWorldController.java
@@ -0,0 +1,14 @@
+package pl.vistersky.helloworld;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+
+    @GetMapping("/hello")
+    public String sayHello() {
+        return "Hello World!!";
+    }
+
+}
